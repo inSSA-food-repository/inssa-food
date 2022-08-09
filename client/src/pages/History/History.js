ad6bd68cfc2bbc692083b42796e3872662937c36
horz.d <he1236@ajou.ac.kr>
2022-08-09 15:03:21 +0900

    History Card Slider 충돌 문제 해결 및 스타일 개선

--- client/src/pages/History/History.js
+++ client/src/pages/History/History.js
@@ -1,7 +1,9 @@
 import HistoryCard from "./components/HistoryCard";
 import dummyData from "./data/dummyData";
 
-import Slider from "react-touch-drag-slider";
+// import Slider from "react-touch-drag-slider";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import "./History.css";
 
@@ -9,43 +11,38 @@ const History = () => {
   const dummy = dummyData.historyCard;
   console.log(dummy[0].id);
 
-  return (
-    <div className="history-container">
-      <div className="card-wrapper">
-        {dummy.map((item, index) => {
-          return (
-            <HistoryCard
-              key={index}
-              id={item?.id}
-              name={item?.name}
-              food_img={item?.food_img}
-              desc={item?.description}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-const History2 = () => {
-  const dummy = dummyData.historyCard;
-  console.log(dummy[0].id);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 374 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 374, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
 
   return (
     <div className="history-container">
-      <Slider
-        className="card-slider"
-        onSlideComplete={(i) => {
-          console.log("finished dragging, current slide is", i);
-        }}
-        onSlideStart={(i) => {
-          console.log("started dragging on slide", i);
-        }}
-        activeIndex={0}
-        transition={0.5}
-        scaleOnDrag={true}
-        
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        // ssr={true} // means to render carousel on server-side.
+        // infinite={true}
+        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        // autoPlaySpeed={1000}
+        // keyBoardControl={true}
+        // customTransition="all .5"
+        transitionDuration={500}
       >
         {dummy.map((item, index) => {
           return (
@@ -58,11 +55,9 @@ const History2 = () => {
             />
           );
         })}
-      </Slider>
+      </Carousel>
     </div>
   );
 };
 
-// export default History;
-
-export default History2;
+export default History;
--- client/src/pages/History/data/dummyData.js
+++ client/src/pages/History/data/dummyData.js
@@ -18,6 +18,51 @@ const dummyData = {
       description:
         "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
     },
+    {
+      id: 2,
+      name: "순댓국",
+      user_id: 2,
+      food_id: 2,
+      food_img: "../data/koreanBBQ.jpg",
+      description:
+        "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
+    },
+    {
+      id: 2,
+      name: "순댓국",
+      user_id: 2,
+      food_id: 2,
+      food_img: "../data/koreanBBQ.jpg",
+      description:
+        "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
+    },
+    {
+      id: 2,
+      name: "순댓국",
+      user_id: 2,
+      food_id: 2,
+      food_img: "../data/koreanBBQ.jpg",
+      description:
+        "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
+    },
+    {
+      id: 2,
+      name: "순댓국",
+      user_id: 2,
+      food_id: 2,
+      food_img: "../data/koreanBBQ.jpg",
+      description:
+        "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
+    },
+    {
+      id: 2,
+      name: "순댓국",
+      user_id: 2,
+      food_id: 2,
+      food_img: "../data/koreanBBQ.jpg",
+      description:
+        "돼지뼈를 푹 고아 우려낸 사골 국물에 순대, 돼지 머릿고기, 염통과 돼지 밥통(오소리감투) 등 각종 내장류를 넣고 다시 한 번 끓여 만든 국밥을 말한다.",
+    },
   ],
 };
 
