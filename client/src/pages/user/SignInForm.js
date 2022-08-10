698ab3a2ecd8c27458c5802a0fa071b01537754e
Ckck12 <100400mjkim@gmail.com>
2022-08-10 19:45:47 +0900

    로그인 기능
    무비 앱으로 데이터 들어가는거 러닝푸드DB로 바꿔야함

--- client/src/pages/user/SignInForm.js
+++ client/src/pages/user/SignInForm.js
@@ -1,80 +1,89 @@
-import React, { useState } from "react";
 import $ from 'jquery';
-import axios from "axios";
-import port from './../../data/port.json'
-import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import port from './../../data/port.json';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 import { useCookies } from 'react-cookie';
 
+const SignInForm = ({ signInData, onChangeSignInData, setSignInData }) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['userData']);
 
-const SigninForm = ( {signInData, onSignInChange} ) => {
-    
-    const navigate = useNavigate();
-    
-    const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-    const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-
-    const onLoginClick = () => {
-        
-        // 유효성검사 - 빈칸인지
-        if (!signInData.email) {
-            $('#email').focus(); //jQuery 라이브러리 버전 업데이트돼서 줄끄임 -> 그래도 지원은 해주더라.
-            alert('이메일을 입력하세요');
-            return;
-        }
-
-        if (!signInData.password) {
-            $('#password').focus();
-            alert('비밀번호를 입력하세요');
-            return;
-        }
-        
-        console.log('로그인 요청');
-
-
-        // 데이터 post 요청 보내기 - axios. DB에 접속하기 때문에 await 사용.
-        sendSignInData()
-        .then(
-            res => {
-                    console.log(res);
-                    setCookie("userData", res.data, { path: "/" });
-                    console.log(cookies.userData);
-                    alert('로그인이 완료되었습니다.');
-                    navigate('review/list');
-                    
-                    }
-            )
-        .catch(e => {
-            setErrorMessage(e.response.data.fail);
-            });
-    }
-    
-    const sendSignInData = async () => {
-        return await axios.post(`${port.url}/user/login`, signInData );
+  const onClickLoginButton = () => {
+    if (signInData.email === '') {
+      alert('이메일을 입력해주세요.');
+      $('#email').focus();
+      return;
     }
 
+    if (signInData.password === '') {
+      alert('비밀번호를 입력해주세요.');
+      $('#password').focus();
+      return;
+    }
 
-    return (
-    <div className="container">
-        <div className="album">
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" onChange={onSignInChange} value={signInData.email} className="form-control" id="email" name="email" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" onChange={onSignInChange} value={signInData.password} className="form-control" id="password" name="password" />
-                </div>
-                <div className="mb-3">
-                    <p className="text-danger">{errorMessage}</p>
-                </div>
-                <button type="button" onClick={onLoginClick} className="btn btn-primary">로그인</button>
-            </form>
-        </div> 
+    sendSignInData()
+      .then((res) => {
+        console.log(res);
+        setCookie('userData', res.data, { path: '/' });
+        alert('로그인이 완료되었습니다.');
+        navigate('/review/list');
+      })
+      .catch((e) => {
+        console.log(e);
+        setErrorMessage(e.response.data.fail);
+      });
+  };
+
+  const sendSignInData = async () => {
+    return await axios.post(port.url + '/user/login', signInData);
+  };
+
+  return (
+    <div className='album'>
+      <div className='container'>
+        <form>
+          <div className='mb-3'>
+            <label htmlFor='email' className='form-label'>
+              Email address
+            </label>
+            <input
+              type='email'
+              value={signInData.email}
+              onChange={onChangeSignInData}
+              className='form-control'
+              name='email'
+              id='email'
+              aria-describedby='emailHelp'
+            />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='password' className='form-label'>
+              Password
+            </label>
+            <input
+              type='password'
+              value={signInData.password}
+              onChange={onChangeSignInData}
+              name='password'
+              className='form-control'
+              id='password'
+            />
+          </div>
+          <div className='mb-3'>
+            <p className='text-danger'>{errorMessage}</p>
+          </div>
+          <button type='button' onClick={onClickLoginButton} className='btn btn-primary'>
+            로그인
+          </button>
+        </form>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default SigninForm;
+export default SignInForm;
